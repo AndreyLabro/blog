9fc402bcc40fe4ca5d1b4cd2f40b4f6e1f57e015
AndreyLabro <rubycodelab@gmail.com>
2016-04-14 17:17:43 +0300

    change contacts/new to /contacts

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   
   get '/' => 'home#index'
   
-  resource :contacts, only: [:new, :create]
+  # get 'contacts' => 'contacts#new' # простой вариант вывода страницы с get
+  resource :contacts, only: [:new, :create], path_names: {:new => ''} #другой способ вывода get
   resources :articles
  
   # The priority is based upon order of creation: first created -> highest priority.
