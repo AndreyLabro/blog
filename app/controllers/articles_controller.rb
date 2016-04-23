b7cf33f5f26e1e611e308a247b33104f7c8cba8b
AndreyLabro <rubycodelab@gmail.com>
2016-04-23 13:32:59 +0300

    article#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,10 +3,15 @@ class ArticlesController < ApplicationController
   def new
   end
   
+  def show
+    @article = Article.find(params[:id])
+  end
+  
   def create
     @article = Article.new(article_params)
     if @article.valid?
       @article.save
+      redirect_to @article
     else
       render action: 'new'
     end
