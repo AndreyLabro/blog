45cd14841a48ed5d89b7195acee34d587470fcdd
AndreyLabro <rubycodelab@gmail.com>
2016-05-01 17:49:40 +0300

    article presence tests. logic group of tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 RSpec.describe Article, type: :model do
-  it { should have_many :comments }
+  describe "associations" do
+    it { should have_many :comments }
+  end
+  describe "validations" do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :text }
+  end
 end
