e95577f8e00ef822e52e8a68c6f87f1b1f9149de
AndreyLabro <rubycodelab@gmail.com>
2016-05-03 13:32:37 +0300

    sen message test added

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -5,4 +5,13 @@ feature "Contact creation" do
     visit '/contacts'
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
+
+  scenario "Allows guest to create contact" do
+    visit '/contacts'
+    fill_in :contact_email, :with => 'user@sample.com'
+    fill_in :contact_message, :with => 'user@sample.com'
+    click_button 'Yarrr!'
+
+    expect(page).to have_content I18n.t('contacts.send_success')
+  end
 end
