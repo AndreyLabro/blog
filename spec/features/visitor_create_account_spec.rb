5ddc577cbfd94204cf577fcc18f2148db4dd8405
AndreyLabro <rubycodelab@gmail.com>
2016-05-03 13:52:22 +0300

    fix deprcated devise message

--- spec/features/visitor_create_account_spec.rb
+++ spec/features/visitor_create_account_spec.rb
@@ -5,8 +5,8 @@ feature "Account creation" do
   scenario "Allows guest to create account" do
     visit new_user_registration_path
 
-    fill_in :user_email, :with => 'user2@sample2.com'
-    fill_in :user_username, :with => 'username2'
+    fill_in :user_email, :with => 'user23@sample23.com'
+    fill_in :user_username, :with => 'username32'
     fill_in :user_password, :with => 'somepass'
     fill_in :user_password_confirmation, :with => 'somepass'
 
