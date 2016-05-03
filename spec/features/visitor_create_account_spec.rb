f35999f9fa23f804a042b78c50a2c225e959a9e6
AndreyLabro <rubycodelab@gmail.com>
2016-05-03 13:48:55 +0300

    testing user succesful registration

---
+++ spec/features/visitor_create_account_spec.rb
@@ -0,0 +1,18 @@
+require "spec_helper"
+
+feature "Account creation" do
+
+  scenario "Allows guest to create account" do
+    visit new_user_registration_path
+
+    fill_in :user_email, :with => 'user2@sample2.com'
+    fill_in :user_username, :with => 'username2'
+    fill_in :user_password, :with => 'somepass'
+    fill_in :user_password_confirmation, :with => 'somepass'
+
+    click_button 'Sign up'
+
+    expect(page).to have_content 'Welcome! You have signed up successfully'
+  end
+
+end
