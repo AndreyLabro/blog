60c98de99d3d9630c6def5c8f4bb22277e58a228
AndreyLabro <rubycodelab@gmail.com>
2016-05-03 14:20:56 +0300

    test database cleaner added

--- Gemfile
+++ Gemfile
@@ -54,4 +54,5 @@ end
 
 group :test do
   gem 'shoulda-matchers', require: false
+  gem 'database_cleaner'
 end
--- spec/features/visitor_create_account_spec.rb
+++ spec/features/visitor_create_account_spec.rb
@@ -1,18 +1,22 @@
 require "spec_helper"
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :truncation
+DatabaseCleaner.clean
 
 feature "Account creation" do
 
   scenario "Allows guest to create account" do
     visit new_user_registration_path
 
-    fill_in :user_email, :with => 'user23@sample23.com'
-    fill_in :user_username, :with => 'username32'
+    fill_in :user_email, :with => 'user@sample.com'
+    fill_in :user_username, :with => 'username'
     fill_in :user_password, :with => 'somepass'
     fill_in :user_password_confirmation, :with => 'somepass'
 
     click_button 'Sign up'
 
-    expect(page).to have_content 'Welcome! You have signed up successfully'
+    expect(page).to have_content I18n.t('devise.registrations.signed_up')
   end
 
 end
