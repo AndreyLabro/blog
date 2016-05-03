c58a4391d0a953159906e59b1f78a5b12e9e4573
AndreyLabro <rubycodelab@gmail.com>
2016-05-03 12:27:43 +0300

    remove article test with factory girl

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,38 +1,38 @@
 class ArticlesController < ApplicationController
-  
+
   before_filter :authenticate_user!, :only => [:new, :create]
-  
+
   def new
   end
-  
+
   def index
     @article = Article.all
   end
-  
+
   def edit
     @article = Article.find(params[:id])
   end
-  
+
   def destroy
     Article.find(params[:id]).destroy
     #redirect_to :back #just go back
     redirect_to articles_path # Alternative with redirect to articles list
   end
-  
+
   def update
     @article = Article.find(params[:id])
-    
+
     if @article.update(article_params)
       redirect_to @article
     else
       render action: 'edit'
     end
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
-  
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -41,11 +41,11 @@ class ArticlesController < ApplicationController
       render action: 'new'
     end
   end
-  
+
   private
-  
+
   def article_params
     params.require(:article).permit(:title, :text)
   end
-  
+
 end
